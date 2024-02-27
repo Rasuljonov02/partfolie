d9ae9d2549c962b02a7aadccb9f8bbc17890874b
Rasuljonov02 <muhammadjonrasuljonov2002@gmail.com>
2024-02-27 17:13:20 +0500

    start

---
+++ main.js
@@ -0,0 +1,13 @@
+let options = {
+	strings: ["Assalomu Alaykum", "My Name Muhammadjon"],
+	typeSpeed: 50,
+	startDelay: 1000,
+	loop: true,
+	loopCount: Infinity,
+	backSpeed: 80,
+	backDelay: 500,
+	showCursor: true,
+	cursorChar: "",
+};
+
+let typed = new Typed("#sozchiqaradi", options);
