42e0465dcd7d10cd33491b3f4965aced84506eb6
Rasuljonov02 <muhammadjonrasuljonov4202@gmail.com>
2024-03-13 15:59:46 +0500

    f5

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let options = {
-	strings: ["Assalomu Alaykum", "My Name", " Muhammadjon"],
+	strings: ["Hello", "My Name", " Muhammadjon"],
 	typeSpeed: 50,
 	startDelay: 1000,
 	loop: true,
@@ -12,27 +12,28 @@ let options = {
 
 let typed = new Typed("#sozchiqaradi", options);
 
+$(document).ready(function () {
+	$("#contactForm").submit(function (e) {
+		e.preventDefault(); // Prevent the form from submitting normally
 
-$(document).ready(function() {
- $('#contactForm').submit(function(e) {
-     e.preventDefault(); // Prevent the form from submitting normally
+		// Serialize the form data
+		var formData = $(this).serialize();
 
-     // Serialize the form data
-     var formData = $(this).serialize();
-
-     // Send an AJAX request
-     $.ajax({
-         type: 'POST',
-         url: 'your_php_script.php', // Replace 'your_php_script.php' with the actual filename of your PHP script
-         data: formData,
-         success: function(response) {
-             // Display the response message
-             $('#msgSubmit').removeClass('hidden').html(response);
-         },
-         error: function(xhr, status, error) {
-             // Display an error message
-             $('#msgSubmit').removeClass('hidden').text('Oops! Something went wrong and we couldn\'t send your message.');
-         }
-     });
- });
+		// Send an AJAX request
+		$.ajax({
+			type: "POST",
+			url: "your_php_script.php", // Replace 'your_php_script.php' with the actual filename of your PHP script
+			data: formData,
+			success: function (response) {
+				// Display the response message
+				$("#msgSubmit").removeClass("hidden").html(response);
+			},
+			error: function (xhr, status, error) {
+				// Display an error message
+				$("#msgSubmit")
+					.removeClass("hidden")
+					.text("Oops! Something went wrong and we couldn't send your message.");
+			},
+		});
+	});
 });
